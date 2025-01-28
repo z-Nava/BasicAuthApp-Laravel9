d3609e46d0a5b14445e5e3d8d5f64e0b2d689d1c
z-Nava <62721391+z-Nava@users.noreply.github.com>
2025-01-28 12:23:37 -0600

    CAMPO DE USUARIO AGREGADO

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'google2fa_secret'
     ];
 
     /**
@@ -31,6 +32,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'google2fa_secret'
     ];
 
     /**
