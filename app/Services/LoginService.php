143c4aac706131ad4409be26b6c00d23579e1631
z-Nava <62721391+z-Nava@users.noreply.github.com>
2025-01-29 17:56:47 -0600

    DOCUMENTACION DE REGISTRO - ELIMINACION DE TWOFACTORSERVICE

--- app/Services/LoginService.php
+++ app/Services/LoginService.php
@@ -7,24 +7,29 @@ use PragmaRX\Google2FA\Google2FA;
 
 class LoginService
 {
+/**
+ * Servicio para autenticar a un usuario con 2FA.
+*/
     public function authenticate($request)
     {
+        //Autenticar al usuario
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $user = Auth::user();
 
+            //Verificar si el usuario tiene 2FA habilitado
             if ($user->google2fa_secret) {
                 $google2fa = new Google2FA();
-
+            //Verificar si el código 2FA es válido
                 if (!$google2fa->verifyKey($user->google2fa_secret, $request->input('2fa_code'))) {
                     Auth::logout();
-
+                    //Devolver un error si el código 2FA es inválido
                     return [
                         'status' => 'error',
                         'errors' => ['2fa_code' => 'El código 2FA es inválido.']
                     ];
                 }
             }
-
+            //Establecer la sesión de autenticación
             session(['2fa_authenticated' => true]);
             return ['status' => 'success'];
         }
--- app/Services/RegisterService.php
+++ app/Services/RegisterService.php
@@ -8,26 +8,30 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class RegisterService
 {
+/**
+ * Servicio para registrar a un usuario con 2FA.
+*/
     public function registerUser($request)
     {
+    //Crear un nuevo usuario
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
-
+    //Generar una clave secreta para el usuario
         $google2fa = new Google2FA();
         $secretKey = $google2fa->generateSecretKey();
-
+    //Guardar la clave secreta en la base de datos
         $user->google2fa_secret = $secretKey;
         $user->save();
-
+    //Generar un código QR para el usuario
         $qrCodeUrl = $google2fa->getQRCodeUrl(
             'BasicAuthApp',
             $user->email,
             $secretKey
         );
-
+    //Generar el código QR
         $qrCode = QrCode::size(200)->generate($qrCodeUrl);
 
         return [
