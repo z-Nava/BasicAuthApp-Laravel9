ad19904ee493835506753f81c93d9cb3f30307fa
z-Nava <62721391+z-Nava@users.noreply.github.com>
2025-01-29 18:00:31 -0600

    DOCUMENTACION DE TWOFACTORSERVICE

--- app/Services/TwoFactorService.php
+++ app/Services/TwoFactorService.php
@@ -6,11 +6,16 @@ use App\Models\User;
 
 class TwoFactorService
 {
+/**
+ * Servicio para invalidar a un usuario con 2FA.
+*/
     public function invalidate($userId)
     {
+        //Buscar al usuario
         $user = User::find($userId);
-
+        //Eliminar al usuario si no está verificado
         if ($user && !$user->google2fa_verified) {
+            //Eliminar al usuario
             $user->delete();
         }
     }
