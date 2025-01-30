7f0a9f126322a2e6033d01bfeeeb46a596f82704
z-Nava <62721391+z-Nava@users.noreply.github.com>
2025-01-29 18:09:34 -0600

    DOCUMENTACION DE RUTAS

--- routes/web.php
+++ routes/web.php
@@ -13,31 +13,28 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-// Rutas de autenticación
+// Rutas de autenticación de inicio de sesión y registro
 Route::controller(AuthController::class)->group(function () {
-    
+    // Rutas de autenticación
     Route::get('/login', 'showLoginForm')->name('login');
     Route::post('/login', 'login');
     Route::post('/logout', 'logout')->name('logout');
-
+    // Rutas de registro
     Route::get('/register', 'showRegisterForm')->name('register');
     Route::post('/register', 'register');
-
+    // Ruta de invalidación de 2FA
     Route::post('/invalidate-2fa', 'invalidate2FA')->name('invalidate.2fa');
 });
 
-
+    // Rutas de home
 Route::middleware('auth')->group(function () {
-    Route::get('/welcome', function () {
-        return view('welcome');
-    })->name('welcome');
+    Route::get('/welcome', function () { return view('welcome');})->name('welcome');
 
-    Route::get('/', function () {
-        return view('welcome');
+    Route::get('/', function () {return view('welcome');
     });
 });
 
-// 🔹 Manejo de rutas no encontradas
+// Manejo de rutas no encontradas 
 Route::fallback(function () {
     return response()->view('errors.404', [], 404);
 });
